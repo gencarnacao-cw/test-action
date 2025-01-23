186baafbc3455314503b1de062b7adf32292e80f
Gustavo Encarnação <g.encarnacao@compliance-wise.com>
2025-01-23 12:20:20 +0100

    Dumping context

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -l
 
 echo "Peeking: $1"
-env
-ls ${GITHUB_ENV}
-cat ${GITHUB_ENV}
-ls ${GITHUB_STATE}
-cat ${GITHUB_STATE}
+echo "Context/n$GITHUB_CONTEXT"
 time=$(date)
 echo "out-var=$time" >> $GITHUB_OUTPUT
 echo "::error::This didnt work"
