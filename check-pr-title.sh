e12cb2f1d29f485f1762071f318331c5ea2926b4
Gustavo Encarnação <g.encarnacao@compliance-wise.com>
2025-01-23 14:01:32 +0100

    Missing script error

--- check-pr-title.sh
+++ check-pr-title.sh
@@ -5,6 +5,6 @@ PR_URL=$(echo $GITHUB_CONTEXT | jq -r ".event.pull_request.title")
 if [ -z "$PR_TITLE" ]; then
     echo "Found Jira issue ID: $(echo $PR_TITLE | grep -o "GRUB-\d*")
 else
-    echo "::error No Jira issue ID found!
+    echo "::Error: No Jira issue ID found!
     exit 1
 fi
