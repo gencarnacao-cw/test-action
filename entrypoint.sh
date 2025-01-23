200a16d3c27ddd1e70a53c73d496fe17f9b7c738
Gustavo Encarnação <g.encarnacao@compliance-wise.com>
2025-01-23 15:16:43 +0100

    Better script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-echo Hello World!
-
 PR_TITLE=$(echo $GITHUB_CONTEXT | jq -r ".event.pull_request.title")
-
 if [ -z "$PR_TITLE" ]; then
-    echo "Found Jira issue ID: $(echo $PR_TITLE | grep -o 'GRUB-\d*')"
-else
-    echo "::Error: No Jira issue ID found!"
+    echo "Checking PR title ${PR_TITLE} for Jira issue ID..."
+
+    JIRA_ISSUE=$(echo $PR_TITLE | grep -o 'GRUB-\d*')
+    if [ $? ]; then
+        echo "::Info: Found Jira issue ID: $(echo $PR_TITLE | grep -o 'GRUB-\d*')"
+    else
+        echo "::Error: No Jira issue ID found!"
+        exit 1
+    fi
+
+    echo "::Error: Could not parse PR title!"
     exit 1
 fi
